ba33e8a620caab27ab9b60a427a9fc8e7275ecf9
animeshmaiti <a.maiti770@gmail.com>
2023-08-30 23:32:56 +0530

    update

--- sr-ff/design/SRFF.v
+++ sr-ff/design/SRFF.v
@@ -31,22 +31,10 @@ module SRFF(q,qbar,clock,reset,s,r);
         end
     else begin
             case({s,r})
-                2'b00: begin
-                    q<=q;
-                    qbar<=~q;
-                end
-                2'b01: begin
-                    q<=0;
-                    qbar<=1;
-                end
-                2'b10: begin
-                    q<=1;
-                    qbar<=0;
-                end
-                2'b11: begin
-                    q<=1'bx;
-                    qbar<=1'bx;
-                end
+                2'b00: begin q<=q; nqbar<=~q; end
+                2'b01: begin q<=0; qbar<=1; end
+                2'b10: begin q<=1; qbar<=0; end
+                2'b11: begin q<=1'bx; qbar<=1'bx; end
             endcase
          end
     end
